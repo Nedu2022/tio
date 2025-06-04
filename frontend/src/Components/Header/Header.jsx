188cbac7e2052938701deb3e3a9ee88e98d4b241
Nedu2022 <nwabuokeichinedu00@gmail.com>
2025-06-04 19:51:50 +0100

    Home page

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -2,18 +2,46 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="absolute top-0 left-0 w-full z-20 px-4 sm:px-8 md:px-16 py-4 flex justify-between items-center bg-white/10 backdrop-blur-xs text-white">
+    <header className="absolute top-0 left-0 w-full z-20 px-4 sm:px-8 md:px-16 py-4 flex justify-between items-center border-b-[0.5px] border-softBlush text-white">
       <h1 className="text-lg md:text-xl font-bold font-nun tracking-wide">
-        TIO
+       <span className="text-softBlush">T</span>IO
       </h1>
 
       <nav className="space-x-4 md:space-x-8 text-sm md:text-base font-medium">
-        <Link to="/" className="hover:underline hover:text-pink-400 transition-colors duration-200">Home</Link>
-        <Link to="/about" className="hover:underline hover:text-pink-400 transition-colors duration-200">About</Link>
-        <Link to="/services" className="hover:underline hover:text-pink-400 transition-colors duration-200">Book a Session</Link>
-          <Link to="/services" className="hover:underline hover:text-pink-400 transition-colors duration-200">Resources</Link>
-        <Link to="/contact" className="hover:underline hover:text-pink-400 transition-colors duration-200">FAQ</Link>
+        <Link
+          to="/"
+          className="hover:underline hover:text-pink-400 transition-colors duration-200"
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className="hover:underline hover:text-pink-400 transition-colors duration-200"
+        >
+          About
+        </Link>
+        <Link
+          to="/services"
+          className="hover:underline hover:text-pink-400 transition-colors duration-200"
+        >
+          Book a Session
+        </Link>
+        <Link
+          to="/services"
+          className="hover:underline hover:text-pink-400 transition-colors duration-200"
+        >
+          Resources
+        </Link>
+        <Link
+          to="/contact"
+          className="hover:underline hover:text-pink-400 transition-colors duration-200"
+        >
+          FAQ
+        </Link>
       </nav>
+      <button className="flip-hover">
+       <span className="flip-text"> Book an Appointment</span>
+      </button>
     </header>
   );
 };
--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -3,7 +3,8 @@ import mainImg from "../../assets/calm.jpg";
 import Content from "../Content/Content";
 import About from "../About/About";
 import Appoint from "../Appointment/appoint";
-
+import { LuDot } from "react-icons/lu";
+import { IoCheckmarkCircleOutline } from "react-icons/io5";
 
 const Home = () => {
   return (
@@ -17,24 +18,54 @@ const Home = () => {
           className="w-full h-full object-cover"
         />
 
-
         <div className="absolute inset-0 flex items-center justify-end px-4 sm:px-8 md:px-16 z-10">
           <div className="text-white text-right w-full">
+            <span className="flex items-center justify-end mb-6 leading-tight font-nun">
+              <LuDot className="text-xl" />
+              <h1>WELCOME TO TIO</h1>
+            </span>
             <h1 className="text-2xl md:text-5xl font-bold mb-6 leading-tight font-nun">
               Healing Starts
               <br />
               With a Conversation
             </h1>
-            <button className="hover:bg-pink-400 hover:text-white bg-primaryBeige text-primaryCoffee font-bold py-2 px-6 rounded mt-4">
-              Book a Session
+            <p>
+              Are you passionate about helping others on their mental health
+              journey? <br />
+              Join our dedicated team of compassionate professionals.
+            </p>
+            <button className="flip-hover mt-10">
+              <span className="flip-text"> Book an Appointment</span>
             </button>
           </div>
+        </div>
+
+        <div className="border-t-[0.5px] border-softBlush absolute bottom-0 left-1/2 transform -translate-x-1/2 min-w-[1200px] max-w-max flex justify-center items-center gap-24 px-4 py-10 ">
+          <span className="flex items-center gap-2 text-white">
+            <IoCheckmarkCircleOutline className="text-xl" />
+            <p>Individual Counselling</p>
+          </span>
+
+          <span className="flex items-center gap-2 text-white">
+            <IoCheckmarkCircleOutline className="text-xl" />
+            <p>Group Therapy</p>
+          </span>
+
+          <span className="flex items-center gap-2 text-white">
+            <IoCheckmarkCircleOutline className="text-xl" />
+            <p>Psychiatric Help</p>
+          </span>
+
+          <span className="flex items-center gap-2 text-white">
+            <IoCheckmarkCircleOutline className="text-xl" />
+            <p>Online Support</p>
+          </span>
         </div>
       </div>
 
       <Content />
       <About />
-      <Appoint/>
+      <Appoint />
     </div>
   );
 };
